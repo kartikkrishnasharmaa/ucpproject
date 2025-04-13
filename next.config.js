d5fb8c15174dfaedadc1b3c2adac416005c0e759
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2025-04-13 20:54:56 +0530

    latest changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enable App Router
-  },
 };
 
 
--- pages/auth/layout.js
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700">
-        {children}
-      </div>
-    );
-  }
